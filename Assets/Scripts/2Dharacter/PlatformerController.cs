f4477c6f2047b73d58ecee62a7a05cab852cc7fe
Hunter Dyar <bloop@hdyar.com>
2020-02-01 10:58:38 -0500

    start of 2D controller

--- Assets/Scripts/2Dharacter/PlatformerController.cs
+++ Assets/Scripts/2Dharacter/PlatformerController.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class PlatformerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int jumps;
+    Vector2 vel;
+    public LayerMask layerMask;
+    public float gravity;
+    public float movementSpeed;
+    public float jumpForce;
+    public float friction;
     void Start()
     {
-        
+        jumps = 0;
     }
 
-    // Update is called once per frame
+    public void Move(Vector2 dir)
+    {
+        dir.Normalize();
+        //Set velocity x axis to movement speed????
+        vel = new Vector2(dir.x*movementSpeed,vel.y);
+    }
+    public void Jump()
+    {
+        if(jumps < 2)
+        {
+            vel = new Vector2(vel.x,jumpForce);
+        }
+    }
     void Update()
     {
-        
+        //gravity Force
+        vel = vel + new Vector2(0,-gravity)*Time.deltaTime;
+
+        CheckCollision();//also friction
+
+        //Move the player
+        transform.position = transform.position+new Vector3(vel.x,vel.y,0)*Time.deltaTime;
+    }
+    void CheckCollision()
+    {
+        Vector2 nextFrame = new Vector3(vel.x,vel.y)*Time.deltaTime;
+        RaycastHit hitInfo = new RaycastHit();
+        if(Physics.BoxCast(transform.position,transform.localScale/2,nextFrame,out hitInfo,Quaternion.identity,nextFrame.magnitude,layerMask))
+        {
+            Vector3 antiForce = Vector3.Project(vel,hitInfo.normal);
+            CheckFriction(hitInfo.normal);
+            //
+            vel = vel - new Vector2(antiForce.x,antiForce.y);
+
+        }
+    }
+    void CheckFriction(Vector3 frictionNormal)
+    {
+        if(frictionNormal != Vector3.zero)
+        {
+            if(Vector3.Dot(vel,frictionNormal) != 0)
+            {
+                vel = vel+ vel * Vector3.Dot(vel,frictionNormal)*friction;
+            }
+            // vel = vel - vel*friction*frictionAmount;
+        }
     }
 }
--- Assets/Scripts/2Dharacter/PlatformerInput.cs
+++ Assets/Scripts/2Dharacter/PlatformerInput.cs
@@ -2,17 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class P : MonoBehaviour
+[RequireComponent(typeof(PlatformerController))]
+public class PlatformerInput : MonoBehaviour
 {
+
+    PlatformerController pc;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        pc = GetComponent<PlatformerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Jump"))
+        {
+            pc.Jump();
+        }
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            pc.Move(Vector2.right);
+        }
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            pc.Move(Vector2.left);
+        }
     }
 }
