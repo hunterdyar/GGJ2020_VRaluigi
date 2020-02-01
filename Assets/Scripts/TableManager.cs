1c5e56a3e0894f0cb7f775b80e791cc0bd9245e1
George Wang <wqz9822@gmail.com>
2020-02-01 15:03:19 -0500

    generate random table

---
+++ Assets/Scripts/TableManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableManager : MonoBehaviour
+{
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tableState = ScriptableObject.CreateInstance<TableScriptableObj>();
+        tableState.generateRandomLayout();
+        tableState.generateCubes(this.transform, cube);
+        var layout = tableState.getLayout();
+        for(int i = 0; i < layout.GetLength(0); i++) {
+            for(int j = 0; j < layout.GetLength(1); j++) {
+                Debug.Log($"row{i},col{j},cube{layout[i,j]}");
+            } 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private TableScriptableObj tableState;
+}
---
+++ Assets/Scripts/TableScriptableObj.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/TableScriptableObject", order = 1)]
+public class TableScriptableObj : ScriptableObject
+{
+    const int ROW = 10;
+    const int COL = 20;
+    const int DEP = 1;
+    const float cubeSize = 0.1f;
+
+
+    public TableScriptableObj()
+    {
+    }
+
+    public int[,] getLayout()
+    {
+        int[,] res = new int[ROW, COL];
+        for (int i = 0; i < ROW; i++)
+        {
+            for (int j = 0; j < COL; j++)
+            {
+                res[i, j] = voxelStates[i, j, curDep];
+            }
+        }
+        return res;
+    }
+
+    public void generateRandomLayout()
+    {
+        for (int i = 0; i < ROW; i++)
+        {
+            for (int j = 0; j < COL; j++)
+            {
+                voxelStates[i, j, 0] = Random.Range(0, 2);
+            }
+        }
+    }
+
+    public void generateCubes(Transform origin, GameObject cube)
+    {
+        for (int i = 0; i < ROW; i++)
+        {
+            for (int j = 0; j < COL; j++)
+            {
+                for (int k = 0; k < DEP; k++)
+                {
+                    if (voxelStates[i, j, k] == 1)
+                    {
+                        var pos = origin.position + new Vector3(j * cubeSize, i * cubeSize, k * cubeSize);
+                        var newCube = Instantiate(cube, pos, Quaternion.identity);
+                        newCube.transform.parent = origin;
+                    }
+                }
+            }
+        }
+    }
+
+    private int curDep = 0;
+    private int[,,] voxelStates = new int[ROW, COL, DEP];
+
+}
