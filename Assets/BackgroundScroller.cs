9fb7e4f2f50c287cad59a602dad552708e4752f8
Hunter Dyar <bloop@hdyar.com>
2020-02-01 14:35:32 -0500

    scrolling bg

---
+++ Assets/BackgroundScroller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+public class BackgroundScroller : MonoBehaviour
+{
+    private Sprite sprite;
+    public Vector2Reference playerPosition;
+    public float scrollFactor;
+    public int offset = 0;
+    float width = 8;
+    
+    void Awake(){
+        sprite = GetComponent<SpriteRenderer>().sprite;
+    }
+    void Start(){
+
+        width = sprite.bounds.max.x - sprite.bounds.min.x;
+        width = Mathf.Abs(width);
+
+    }
+
+    void Update()
+    {
+        if(playerPosition.Value.x-(width*offset) > 0)
+        {
+            offset++;
+            GameObject.Instantiate(gameObject,transform.position+Vector3.right*width*offset,Quaternion.identity,transform).GetComponent<BackgroundScroller>().enabled = false;
+        }
+      //  transform.position = new Vector3(-playerPosition.Value.x*scrollFactor,transform.position.y,transform.position.x);
+    }
+}
