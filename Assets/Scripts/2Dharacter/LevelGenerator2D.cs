5af84fd5017415c068a78040d10f94b76728bb97
Hunter Dyar <bloop@hdyar.com>
2020-02-01 21:08:24 -0500

    untested 2d level generating and management

--- Assets/Scripts/2Dharacter/LevelGenerator2D.cs
+++ Assets/Scripts/2Dharacter/LevelGenerator2D.cs
@@ -4,26 +4,55 @@ using UnityEngine;
 
 public class LevelGenerator2D : MonoBehaviour
 {
-    public LevelScriptableObj table;
+    public LevelScriptableObj levelState;
     public GameObject blockPrefab;
+    Dictionary<Vector3,GameObject> blocks;
     void Start()
     {
+        blocks = new Dictionary<Vector3,GameObject>();
         //DEBUG
-        table.generateRandomLayout();
+        levelState.generateRandomLayout();
         //
-        GenerateLevel();
+        CreateBlocks();
     }
 
-    [ContextMenu("GenerateLevel")]
-    void GenerateLevel()
+    [ContextMenu("Create Blocks")]
+    public void CreateBlocks()
     {
-        //DEBUG
-        table.generateRandomLayout();
-        foreach(Transform c in transform)
+        //
+        //
+        int k = 0;//What depth is the level at?
+        for (int i = 0; i < levelState.Row; i++)
         {
-            DestroyImmediate(c.gameObject);//destroy all children NOW.
+            for (int j = 0; j < levelState.Col; j++)
+            {
+                    Vector3 key = levelState.getCubePos(i, j, k);
+                    if(blocks.ContainsKey(key))
+                    {
+                        //a block exists here.
+                        //is it the right type of block?
+                    }else
+                    {
+                        GameObject newBlock = Instantiate(blockPrefab, key, Quaternion.identity);
+                        newBlock.transform.parent = this.transform;//SetParent?
+                        blocks[key] = newBlock;//add to the dictionary
+                    }
+            }
         }
         //
-        // table.generateCubes(transform,blockPrefab,1);
+        foreach(KeyValuePair<Vector3,GameObject> bl in blocks)
+        {
+            List<Vector3> removeMe = new List<Vector3>();
+            //If there is no block at this position anymore, we need to destory it.
+            if(!levelState.hasCube((int)bl.Key.x,(int)bl.Key.y,(int)bl.Key.z))
+            {
+                removeMe.Add(bl.Key);
+            }
+            foreach(Vector3 b1 in removeMe)
+            {
+                Destroy(blocks[b1]);
+                blocks.Remove(b1);
+            }
+        }
     }
 }
