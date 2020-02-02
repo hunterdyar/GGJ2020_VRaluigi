685da97104655338858f3b9150d372d668e53cb9
Hunter Dyar <bloop@hdyar.com>
2020-02-02 09:35:55 -0500

    Background Music Player
    Added random options as well, can randomize at start, and/or randomize the list every selection.

---
+++ Assets/Scripts/BGMusic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMusic : MonoBehaviour
+{
+    public bool startWithRandomSong;
+    public bool randomSongEachTurn;
+    public AudioClip[] trackList;
+    
+    int trackIndex = 0;
+    int playingIndex;
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if(startWithRandomSong)
+        {
+            trackIndex = Random.Range(0,trackList.Length-1);
+        }
+        PlaySong(trackIndex);
+    }
+
+    public void PlaySong(int indexToPlay)
+    {
+        audioSource.clip = trackList[indexToPlay];
+        StartCoroutine(WaitForSongEnd(audioSource.clip.length));
+        audioSource.Play();
+        playingIndex = indexToPlay;
+    }
+    IEnumerator WaitForSongEnd(float songLength)
+    {
+        yield return new WaitForSeconds(songLength);
+        if(randomSongEachTurn)
+        {
+            while(trackIndex == playingIndex){//wont repeat itself twice in a row.
+                trackIndex = Random.Range(0,trackList.Length-1);
+            }
+        }
+        else
+        {
+            trackIndex++;
+            if(trackIndex >= trackList.Length)
+            {
+                trackIndex = 0;
+            }
+        }
+        PlaySong(trackIndex);
+    }
+}
